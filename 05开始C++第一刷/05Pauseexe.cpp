8c5b436d0335fc9c39f06f2d721e0a625c3ca6d3
youbingchenyoubing <youbingchenyoubing@163.com>
2024-03-30 12:55:52 +0800

    add five

---
+++ 05开始C++第一刷/05Pauseexe.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/*
+引用cstdlib库
+c表示c语言时代的苦
+std：standard
+lib：library
+*/ 
+#include <cstdlib> 
+using namespace std;  
+int main(){
+ 
+	 int a=10;
+	 cout<<"Hello  World!"<<endl;
+	 system("pause");
+	 return 0;                  
+}
